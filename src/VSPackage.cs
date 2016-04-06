65a7b165a7c93b7eeb2377120d39700a1a2f5e08
Mads Kristensen <post@madskristensen.net>
2016-04-06 13:47:26 -0700

    Added missing NuGet package

--- src/VSPackage.cs
+++ src/VSPackage.cs
@@ -1,38 +1,23 @@
 ﻿using System;
-using System.ComponentModel.Design;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using System.Runtime.InteropServices;
-using Microsoft.VisualStudio;
-using Microsoft.VisualStudio.OLE.Interop;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
-using Microsoft.Win32;
 
 namespace WebAccessibilityChecker
 {
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
-    [Guid(VSPackage.PackageGuidString)]
-    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    [Guid(PackageGuids.guidPackageString)]
     public sealed class VSPackage : Package
     {
-        public const string PackageGuidString = "5123051b-3c1d-40e9-aedd-db11ee9f8b9a";
-
-        public VSPackage()
-        {
-        }
-
-        #region Package Members
-
         protected override void Initialize()
         {
+            Logger.Initialize(this, Vsix.Name);
+            Telemetry.Initialize(this, Vsix.Version, "1c740e68-eead-45bb-a583-0f1cf4c33100");
+
             EnableCommand.Initialize(this);
+
             base.Initialize();
         }
-
-        #endregion
     }
 }
