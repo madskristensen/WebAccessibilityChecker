50d21c2f824c5ead26e04bcafd1f97767a77d5e9
Mads Kristensen <post@madskristensen.net>
2016-04-22 08:48:10 -0700

    Move package auto-load to web project open

--- src/VSPackage.cs
+++ src/VSPackage.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
 
 namespace WebAccessibilityChecker
 {
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
-    [ProvideAutoLoad(UIContextGuids80.SolutionExists)]
     [ProvideOptionPage(typeof(Options), "Web", "Accessibility Checker", 101, 111, true, new[] { "a11y", "wai", "section508", "wcag" }, ProvidesLocalizedCategoryName = false)]
     [Guid(PackageGuids.guidPackageString)]
+    [ProvideAutoLoad("{349C5852-65DF-11dA-9384-00065B846F21}")] // WAP
+    [ProvideAutoLoad("{E24C65DC-7377-472b-9ABA-BC803B73C61A}")] // WebSite
+    [ProvideAutoLoad("{8BB2217D-0F2D-49D1-97BC-3654ED321F3B}")] // ProjectK
     public sealed class VSPackage : Package
     {
         public static Options Options { get; private set; }
