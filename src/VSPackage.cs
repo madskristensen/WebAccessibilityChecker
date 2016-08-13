eed6411abe5ac5bf2b2e905e5b303c5b45179dc5
Mads Kristensen <post@madskristensen.net>
2016-08-13 15:13:10 -0700

    Async package load

--- src/VSPackage.cs
+++ src/VSPackage.cs
@@ -5,13 +5,13 @@ using Microsoft.VisualStudio.Shell;
 namespace WebAccessibilityChecker
 {
     [Guid(PackageGuids.guidPackageString)]
-    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading =true)]
     [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [ProvideOptionPage(typeof(Options), "Web", "Accessibility Checker", 101, 111, true, new[] { "a11y", "wai", "section508", "wcag" }, ProvidesLocalizedCategoryName = false)]
-    [ProvideAutoLoad("{349C5852-65DF-11dA-9384-00065B846F21}")] // WAP
-    [ProvideAutoLoad("{E24C65DC-7377-472b-9ABA-BC803B73C61A}")] // WebSite
-    [ProvideAutoLoad("{8BB2217D-0F2D-49D1-97BC-3654ED321F3B}")] // ProjectK
+    [ProvideAutoLoad("{349C5852-65DF-11dA-9384-00065B846F21}", PackageAutoLoadFlags.BackgroundLoad)] // WAP
+    [ProvideAutoLoad("{E24C65DC-7377-472b-9ABA-BC803B73C61A}", PackageAutoLoadFlags.BackgroundLoad)] // WebSite
+    [ProvideAutoLoad("{8BB2217D-0F2D-49D1-97BC-3654ED321F3B}", PackageAutoLoadFlags.BackgroundLoad)] // ProjectK
     public sealed class VSPackage : Package
     {
         public static Options Options { get; private set; }
